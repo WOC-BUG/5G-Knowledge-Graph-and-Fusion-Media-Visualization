441eb0f39f773fb3fe360dc2d1bceff646f76689
WOC_BUG <1539777243@qq.com>
2020-08-04 19:52:44 +0800

    index page

--- active.js
+++ active.js
@@ -2,8 +2,8 @@ window.onload=function(){
     var Div  =  document.getElementById('div1');        //返回 ID 为div1的第一个对象的引用
     var Ul = document.getElementsByTagName('Ul')[0];
     var Li = Ul.getElementsByTagName('li');             //获取 Ul下的所有li
-    Ul.innerHTML = Ul.innerHTML+Ul.innerHTML;           //li下的内容相加
-    Ul.style.width = Li[0].offsetWidth*Li.length+'px';  //Ul的宽度等于每个li的宽度乘以所有li的长度
+    Ul.innerHTML = Ul.innerHTML+Ul.innerHTML;           //Ul标签内的代码，变成两份接在一起
+    Ul.style.width = Li[0].offsetWidth*Li.length+'px';  //Ul的宽度等于每个li的宽度乘以所有li的长度，+px表示单位为px
     var speed = 2;
     
     //主方法
@@ -11,7 +11,7 @@ window.onload=function(){
         //如果左边横向滚动了长度一半之后,回到初始位置
         
         if(Ul.offsetLeft<-Ul.offsetWidth/speed){
-            Ul.style.left = '0'
+            Ul.style.left = '0';
         }
         //如果右边横向滚动的距离大于0 就让他的位置回到一半
         if(Ul.offsetLeft>0){
