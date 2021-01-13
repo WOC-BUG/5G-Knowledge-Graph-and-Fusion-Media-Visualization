76c1b5fba5ae17151f765f0dd4b5db281b161461
WOC_BUG <1539777243@qq.com>
2021-01-13 22:07:20 +0800

    用Vue框架重写

---
+++ Vue框架重写/main.js
@@ -0,0 +1,9 @@
+var app=new Vue({
+    el: '#app',
+    data:{
+        activeIndex:1,
+        mainImg:["./img/view.jpg","./img/sea.jpg","./img/sky.jpg","./img/school.jpg"],
+        imgNumber:4
+
+    }
+})
