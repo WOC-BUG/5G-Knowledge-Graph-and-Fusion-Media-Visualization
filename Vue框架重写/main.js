a6905a01227bf5ea3e1e3ff20fa2278dc7d9c032
WOC_BUG <1539777243@qq.com>
2021-01-14 17:37:41 +0800

    修改布局，添加动画

--- Vue框架重写/main.js
+++ Vue框架重写/main.js
@@ -3,7 +3,26 @@ var app=new Vue({
     data:{
         activeIndex:1,
         mainImg:["./img/view.jpg","./img/sea.jpg","./img/sky.jpg","./img/school.jpg"],
-        imgNumber:4
+        imgNumber:4,
+        fits: ['fill', 'contain', 'cover', 'none', 'scale-down'],
+        scroll:'',
+        isShowImg:false,
+        isShowVedio:false,
+    },
+    methods: {
+        menu() {
+            this.scroll = document.documentElement.scrollTop || document.body.scrollTop;
+            console.log(this.scroll);
+            this.isShowImg=false;
+            this.isShowVedio=false;
 
-    }
+            if(this.scroll>100)
+                this.isShowImg=true;
+            if(this.scroll>700)
+                this.isShowVedio=true;
+        }
+      },
+    mounted() {
+        window.addEventListener('scroll', this.menu)
+    },
 })
