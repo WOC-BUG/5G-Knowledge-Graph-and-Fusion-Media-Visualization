5df244badc7a899e27294df435ca7a09321a56b3
WOC_BUG <1539777243@qq.com>
2021-02-23 20:42:28 +0800

    修改cloud页面背景及格式

--- 网页/js/cloud.js
+++ 网页/js/cloud.js
@@ -8,6 +8,7 @@ var vedios=new Vue({
         fits: ['fill', 'contain', 'cover', 'none', 'scale-down'],
         cloudUrl:["./img/P2.jpg","./img/B.jpg","./img/C.jpg","./img/D.jpg","./img/E.jpg","./img/F.jpg","./img/G.jpg","./img/H.jpg","./img/I.jpg","./img/I2.jpg","./img/J.jpg","./img/K.jpg","./img/L.jpg","./img/M.jpg","./img/N.jpg","./img/O.jpg","./img/O2.jpg","./img/P.jpg","./img/A.jpg"],
         pdfUrl:["./pdf/P2.pdf","./pdf/B.pdf","./pdf/C.pdf","./pdf/D.pdf","./pdf/E.pdf","./pdf/F.pdf","./pdf/G.pdf","./pdf/H.pdf","./pdf/I.pdf","./pdf/I2.pdf","./pdf/J.pdf","./pdf/K.pdf","./pdf/L.pdf","./pdf/M.pdf","./pdf/N.pdf","./pdf/O.pdf","./pdf/O2.pdf","./pdf/P.pdf","./pdf/A.pdf"],
+        title:["5G广播技术方案","THE NEXT-GENERATION PROTOCOL STACK","INTEGRATION OF SENSING, COMMUNICATION AND COMPUTING TOWARD 6G","WIRELESS TECHNOLOGY TRENDS TOWARDS 6G","6G新天线技术","PRELIMINARY STUDY OF ADVANCED TECHNOLOGIES TOWARDS 6G ERA: QITS 2020","6G SERVICES AND REQUIREMENTS","空天地一体化通信系统","KEY TECHNOLOGIES OF 6G WBAN","ICDT INTEGRATED 6G NETWORK","面向云网融合服务的6G网络技术","以终端为中心的5G安全体系","5G毫米波技术","5G V2X应用场景第二阶段","NR V2X关键技术","5G广播技术应用","USE CASES OF 5G BROADCAST TECHNOLOGY","5G BROADCAST SOLUTION","DATA DRIVEN AND INTENT AWARE SMART WIRELESS NETWORK"]
     },
     methods:{
         onSubmit:function(){
@@ -19,7 +20,7 @@ var vedios=new Vue({
             if(this.scroll>this.pos&&this.count<this.sum)
             {
                 this.count++;
-                this.pos+=600;
+                this.pos+=700;
             }
         }
     },
