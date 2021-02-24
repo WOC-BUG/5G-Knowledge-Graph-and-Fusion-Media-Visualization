c9a9dcc40cc36e029019ac6a72a10a7b340aa12b
WOC_BUG <1539777243@qq.com>
2021-02-24 20:13:07 +0800

    更新子页面背景及布局

--- 网页/js/cloud.js
+++ 网页/js/cloud.js
@@ -20,7 +20,7 @@ var vedios=new Vue({
             if(this.scroll>this.pos&&this.count<this.sum)
             {
                 this.count++;
-                this.pos+=700;
+                this.pos+=600;
             }
         }
     },
