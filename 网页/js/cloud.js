0e8087fdea3f67643e871ff72aab92bd78578551
WOC_BUG <1539777243@qq.com>
2021-03-12 15:05:01 +0800

    再简化压缩图片

--- 网页/js/cloud.js
+++ 网页/js/cloud.js
@@ -6,8 +6,8 @@ var vedios=new Vue({
         pos:5,
         scroll:'',
         fits: ['fill', 'contain', 'cover', 'none', 'scale-down'],
-        cloudUrl:["./img/P2-min.jpg","./img/B-min.jpg","./img/C-min.jpg","./img/D-min.jpg","./img/E-min.jpg","./img/F-min.jpg","./img/G-min.jpg","./img/H-min.jpg","./img/I-min.jpg","./img/I2-min.jpg","./img/J-min.jpg","./img/K-min.jpg","./img/L-min.jpg","./img/M-min.jpg","./img/N-min.jpg","./img/O-min.jpg","./img/O2-min.jpg","./img/P-min.jpg","./img/A-min.jpg"],
-        pdfUrl:["./pdf/P2.pdf","./pdf/B.pdf","./pdf/C.pdf","./pdf/D.pdf","./pdf/E.pdf","./pdf/F.pdf","./pdf/G.pdf","./pdf/H.pdf","./pdf/I.pdf","./pdf/I2.pdf","./pdf/J.pdf","./pdf/K.pdf","./pdf/L.pdf","./pdf/M.pdf","./pdf/N.pdf","./pdf/O.pdf","./pdf/O2.pdf","./pdf/P.pdf","./pdf/A.pdf"],
+        cloudUrl:["../img/P2-min.jpg","../img/B-min.jpg","../img/C-min.jpg","../img/D-min.jpg","../img/E-min.jpg","../img/F-min.jpg","../img/G-min.jpg","../img/H-min.jpg","../img/I-min.jpg","../img/I2-min.jpg","../img/J-min.jpg","../img/K-min.jpg","../img/L-min.jpg","../img/M-min.jpg","../img/N-min.jpg","../img/O-min.jpg","../img/O2-min.jpg","../img/P-min.jpg","../img/A-min.jpg"],
+        pdfUrl:["../pdf/P2.pdf","../pdf/B.pdf","../pdf/C.pdf","../pdf/D.pdf","../pdf/E.pdf","../pdf/F.pdf","../pdf/G.pdf","../pdf/H.pdf","../pdf/I.pdf","../pdf/I2.pdf","../pdf/J.pdf","../pdf/K.pdf","../pdf/L.pdf","../pdf/M.pdf","../pdf/N.pdf","../pdf/O.pdf","../pdf/O2.pdf","../pdf/P.pdf","../pdf/A.pdf"],
         title:["5G广播技术方案","THE NEXT-GENERATION PROTOCOL STACK","INTEGRATION OF SENSING, COMMUNICATION AND COMPUTING TOWARD 6G","WIRELESS TECHNOLOGY TRENDS TOWARDS 6G","6G新天线技术","PRELIMINARY STUDY OF ADVANCED TECHNOLOGIES TOWARDS 6G ERA: QITS 2020","6G SERVICES AND REQUIREMENTS","空天地一体化通信系统","KEY TECHNOLOGIES OF 6G WBAN","ICDT INTEGRATED 6G NETWORK","面向云网融合服务的6G网络技术","以终端为中心的5G安全体系","5G毫米波技术","5G V2X应用场景第二阶段","NR V2X关键技术","5G广播技术应用","USE CASES OF 5G BROADCAST TECHNOLOGY","5G BROADCAST SOLUTION","DATA DRIVEN AND INTENT AWARE SMART WIRELESS NETWORK"]
     },
     methods:{
--- 网页/js/vedios.js
+++ 网页/js/vedios.js
@@ -5,7 +5,7 @@ var vedios=new Vue({
         count: 1,
         pos:20,
         scroll:'',
-        vediosUrl:["./img/中国工程院院士邬贺铨：5G技术与应用.mp4","./img/5G，从流量到价值.mp4","./img/周鸿祎：5G时代的安全挑战与应对.mp4","./img/陆建华：5G发展引发的若干基础研究布局思考.mp4"]
+        vediosUrl:["../img/中国工程院院士邬贺铨：5G技术与应用.mp4","../img/5G，从流量到价值.mp4","../img/周鸿祎：5G时代的安全挑战与应对.mp4","../img/陆建华：5G发展引发的若干基础研究布局思考.mp4"]
     },
     methods:{
         onSubmit:function(){
