152a8d65c771339e27ade35ecf2b04ba31bb065c
WOC_BUG <1539777243@qq.com>
2021-03-15 15:06:08 +0800

    修改动图

--- 网页/js/loading.js
+++ 网页/js/loading.js
@@ -1,7 +1,28 @@
-//显示加载动画
-$(".gif").css("display","flex");
-//调用ajax发送请求
-esdpec.framework.core.getJsonResult('common/getalarmtree', function (res) {
-    //请求成功后再次隐藏加载动画
-    $(".gif").css("display","none");
-})
+
+// 添加事件  
+document.onreadystatechange = StateChangeFun;  
+  
+// 页面状态发生改变时调用的函数  
+function StateChangeFun(){  
+    if(document.readyState == "complete"){  
+        document.forms[0].onsubmite = FormSubmitFun;  
+        document.body.onresize = ReSetSizeFun;  
+  
+        loadingDiv.style.display = "none";  
+          
+    }else  
+    {  
+        ReSetSizeFun();  
+    loadingDiv.style.display = "";  
+  }  
+}  
+// 表单提交时调用  
+function FormSubmitFun(){  
+    ReSetSizeFun();  
+    loadingDiv.style.display = "";  
+ }  
+ // 页面大小时调用  
+function ReSetSizeFun(){  
+    loadingDiv.style.top= document.documentElement.scrollTop + "px";  
+    loadingDiv.style.left= document.documentElement.scrollLeft + "px";  
+} 
