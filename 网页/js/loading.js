96677b6071bd732e63f217e921da246aff38f570
WOC_BUG <1539777243@qq.com>
2021-03-15 15:19:23 +0800

    改为5s定时消失

--- 网页/js/loading.js
+++ 网页/js/loading.js
@@ -1,28 +1,4 @@
-
-// 添加事件  
-document.onreadystatechange = StateChangeFun;  
-  
-// 页面状态发生改变时调用的函数  
-function StateChangeFun(){  
-    if(document.readyState == "complete"){  
-        document.forms[0].onsubmite = FormSubmitFun;  
-        document.body.onresize = ReSetSizeFun;  
-  
-        loadingDiv.style.display = "none";  
-          
-    }else  
-    {  
-        ReSetSizeFun();  
-    loadingDiv.style.display = "";  
-  }  
-}  
-// 表单提交时调用  
-function FormSubmitFun(){  
-    ReSetSizeFun();  
-    loadingDiv.style.display = "";  
- }  
- // 页面大小时调用  
-function ReSetSizeFun(){  
-    loadingDiv.style.top= document.documentElement.scrollTop + "px";  
-    loadingDiv.style.left= document.documentElement.scrollLeft + "px";  
-} 
+setTimeout(function(){//黑衣探花定时器 
+$(".gif").css("display","none");//将图片的display属性设置为none
+},
+5000);//设置5000毫秒即5秒
