457a604eb18f62fbde4fe63b9585b2a869cdce02
WOC_BUG <1539777243@qq.com>
2021-04-04 11:46:00 +0800

    添加视频

--- 网页/js/vedios.js
+++ 网页/js/vedios.js
@@ -1,11 +1,11 @@
 var vedios=new Vue({
     el:"#vedios",
     data:{
-        sum:4,
+        sum:5,
         count: 1,
         pos:20,
         scroll:'',
-        vediosUrl:["../img/中国工程院院士邬贺铨：5G技术与应用.mp4","../img/5G，从流量到价值.mp4","../img/周鸿祎：5G时代的安全挑战与应对.mp4","../img/陆建华：5G发展引发的若干基础研究布局思考.mp4"]
+        vediosUrl:["../img/5G.mp4","../img/中国工程院院士邬贺铨：5G技术与应用.mp4","../img/5G，从流量到价值.mp4","../img/周鸿祎：5G时代的安全挑战与应对.mp4","../img/陆建华：5G发展引发的若干基础研究布局思考.mp4"]
     },
     methods:{
         onSubmit:function(){
